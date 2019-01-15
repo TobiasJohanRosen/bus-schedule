edc431acd32724bd7e4f1678988714968b5b4087
Maximilian Wendel <maximilian.wendel@mwtdev.se>
2019-01-15 15:44:42 +0100

    Added sneaky files that slipped passed git add

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MatButtonModule, MatCardModule } from '@angular/material';
+
+@NgModule({
+  declarations: [],
+  imports: [CommonModule],
+  exports: [MatButtonModule, MatCardModule]
+})
+export class SharedModule {}
