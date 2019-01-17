90c27259f3cfad9c5a0937df24422743fa4fc87d
Maximilian Wendel <maximilian.wendel@mwtdev.se>
2019-01-17 16:00:02 +0100

    Added background image

--- src/app/transit-departure.ts
+++ src/app/transit-departure.ts
@@ -6,56 +6,56 @@ export class TransitDeparture {
   public departing: number;
   public delayedDeparting: number | null;
   public deviations: Array<string> = [];
-  public type: "city" | "regional" | "unknown";
-  public color: string = "#FFFFFF";
+  public type: 'city' | 'regional' | 'unknown';
+  public color: string = '#FFFFFF';
   public departingTime: string;
   public delayedDepartingTime: string | null;
 
   // Initialize new TransitDeparture
   constructor(raw: object) {
-    let rawLine = raw["routeLinks"][0]["line"];
+    let rawLine = raw['routeLinks'][0]['line'];
 
     // Begin parsing
-    this.line = rawLine["lineNo"];
-    this.departure = new Date(raw["departureDateTime"]);
+    this.line = rawLine['lineNo'];
+    this.departure = new Date(raw['departureDateTime']);
     this.type =
-      rawLine["trafficType"] === 1
-        ? "city"
-        : String(rawLine["trafficType"]).match(/3|6/)
-        ? "regional"
-        : "unknown";
-    this.name = rawLine["towards"];
+      rawLine['trafficType'] === 1
+        ? 'city'
+        : String(rawLine['trafficType']).match(/3|6/)
+        ? 'regional'
+        : 'unknown';
+    this.name = rawLine['towards'];
 
     // Format name
     if (this.name.match(/(\svia\s)/)) {
       this.name = this.name.split(/(\svia\s)/)[0];
     }
 
-    this.deviations = raw["routeLinks"][0]["deviations"];
+    this.deviations = raw['routeLinks'][0]['deviations'];
 
     // Set delayedDeparture if it deviates from the initial departure
     if (
-      raw["departureDateTime"] !== raw["routeLinks"][0]["departureDateTime"]
+      raw['departureDateTime'] !== raw['routeLinks'][0]['departureDateTime']
     ) {
       this.delayedDeparture = new Date(
-        raw["routeLinks"][0]["departureDateTime"]
+        raw['routeLinks'][0]['departureDateTime']
       );
     }
 
     this.departingTime = `${
-      this.departure.getHours() < 10 ? "0" : ""
+      this.departure.getHours() < 10 ? '0' : ''
     }${this.departure.getHours()}:${
-      this.departure.getMinutes() < 10 ? "0" : ""
+      this.departure.getMinutes() < 10 ? '0' : ''
     }${this.departure.getMinutes()}`;
 
     // Set delayedDepartingTime if departure deviates from the initial departure
     if (
-      raw["departureDateTime"] !== raw["routeLinks"][0]["departureDateTime"]
+      raw['departureDateTime'] !== raw['routeLinks'][0]['departureDateTime']
     ) {
       this.delayedDepartingTime = `${
-        this.delayedDeparture.getHours() < 10 ? "0" : ""
+        this.delayedDeparture.getHours() < 10 ? '0' : ''
       }${this.delayedDeparture.getHours()}:${
-        this.delayedDeparture.getMinutes() < 10 ? "0" : ""
+        this.delayedDeparture.getMinutes() < 10 ? '0' : ''
       }${this.delayedDeparture.getMinutes()}`;
     }
 
@@ -72,14 +72,14 @@ export class TransitDeparture {
 
     // Set bus color
     switch (this.type) {
-      case "city":
-        this.color = "#30921C";
+      case 'city':
+        this.color = '#30921C';
         break;
-      case "regional":
-        this.color = "#DBBD2C";
+      case 'regional':
+        this.color = '#DBBD2C';
         break;
       default:
-        this.color = "#FFFFFF";
+        this.color = '#FFFFFF';
     }
   }
 }
